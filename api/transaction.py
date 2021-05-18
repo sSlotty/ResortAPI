78388be3d1a0841063d7cabe372068d9ce827449
Oatwant <thanathip.oat@g.swu.ac.th>
2021-05-19 05:38:26 +0700

    fix key  to string

--- api/transaction.py
+++ api/transaction.py
@@ -32,7 +32,7 @@ class TransactionAPI(Resource):
             if len(room_id) > 0:
                 key = uuid.uuid4().int
                 data = {
-                    'transactionID': str(key)[0,6],
+                    'transactionID': str(key)[0:6],
                     'userID': body['userID'],
                     'guestID': body['guestID'],
                     'roomID': body['roomID'],
